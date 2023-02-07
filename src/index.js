d251d41f723389661d60c24431c3a20f0ff50057
Oleksandr Chernomorets <achernomorez@gmail.com>
2023-02-07 12:21:04 +0300

    baseName

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App  from 'components/App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <React.StrictMode basename="/https://alexanderchernomorets.github.io/goit-react-hw-03-phonebook/">
     <App />
   </React.StrictMode>
 );
